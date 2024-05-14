ef42cb101153e46cf5d6d703a237b2c1ac7a7cfb
John Harrison <john.harrison147@virginmedia.com>
2024-05-14 14:18:46 +0100

    tidied up the routes and app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-const express = require('express')
-import {Request, Response} from "express";
+import express from 'express'
 import {router} from './routes'
+//potentially need cors here
 
-const cors = require('cors')
 const app = express()
 const port=3000
-app.listen(port, () => {
-    console.log(`express server runnin at ${port}`)
-})
+
+app.listen(port)
 
 app.use('/', router)
