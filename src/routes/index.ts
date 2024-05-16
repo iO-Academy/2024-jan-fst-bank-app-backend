fb633634c7443ee75e9948f6f8f91e336576a2ae
Izayah Jordan <izayahjordan13@gmail.com>
2024-05-16 12:24:10 +0100

    added missing files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,14 @@ export const router = express.Router()
 import registerController from "../controllers/registerController";
 import tokenMiddleware from "../middleware/tokenMiddleware";
 import loginController from "../controllers/loginController";
-import transactionController from "../controllers/transactionController";
+import transactionController, {transactionHistory} from "../controllers/transactionController";
 import customerController from "../controllers/customerController";
 
 router.post('/register', registerController);
 router.post('/login', loginController)
 router.get('/user/:id', tokenMiddleware, customerController)
 router.post('/transaction', transactionController)
+router.post('/transactionHistory', transactionHistory)
 router.get('/', (req: Request, res: Response) => {
     res.send("hello from index");
 });
