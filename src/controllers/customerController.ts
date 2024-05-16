8bf2735edcdd0781502846086705b70f26b6b127
richardjgabb <richardjgabb@.com>
2024-05-16 10:53:58 +0100

    changed array length

--- src/controllers/customerController.ts
+++ src/controllers/customerController.ts
@@ -1,13 +1,12 @@
 import {getAccountsByCustomerNumber} from "../models/accountModel";
 import { Request, Response } from "express";
-import {Account} from "../models/accountModel";
 
 const customerController = async (req: Request, res: Response) => {
     const customer_number = req.params.id
     const accounts  = await getAccountsByCustomerNumber(customer_number)
     // @ts-ignore
-    if (accounts.length < 1) {
-        res.status(200).send({'Accounts': accounts})
+    if (accounts.length > 0) {
+        res.status(200).send({"success": true, 'accounts': accounts})
     } else {
         res.status(401).send({'message': 'No Accounts Found'})
     }
