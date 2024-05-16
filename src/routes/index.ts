9a6b5a4fa63b51b38808b8425ffce8e06e0c3a5a
Izayah Jordan <izayahjordan13@gmail.com>
2024-05-16 11:14:48 +0100

    added transaction recording with controller route and model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ export const router = express.Router()
 import registerController from "../controllers/registerController";
 import tokenMiddleware from "../middleware/tokenMiddleware";
 import loginController from "../controllers/loginController";
+import transactionController from "../controllers/transactionController";
 
 router.post('/register', registerController);
 router.post('/login', loginController)
+router.post('/transaction', transactionController)
 router.get('/', tokenMiddleware, (req: Request, res: Response) => {
     res.send("hello from index");
 });
