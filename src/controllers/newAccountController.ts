c116bf55bed87ca2d77348d93d1704b61e19baff
richardjgabb <richardjgabb@.com>
2024-05-16 11:26:17 +0100

    linked account number generated

--- src/controllers/newAccountController.ts
+++ src/controllers/newAccountController.ts
@@ -2,15 +2,17 @@ import { User } from '../models/customerModel'
 import { createAccount } from "../models/accountModel";
 import { Account } from "../models/accountModel"
 import { addMoney } from "../models/accountModel";
+import generateAccountNumber from "../utilities/generateAccountNumber";
 
 const buildAccountObject = (user: User): Account => {
+    const accountNumber = generateAccountNumber
     return {
         customer_id: user.customer_number,
         account_type: 'standard',
         balance: 0,
         interest_rate: 0,
         custom_label: 'Main Account',
-        account_number: 3  // Placeholder for function - remember to change!
+        account_number: accountNumber  // Placeholder for function - remember to change!
     }
 }
 
