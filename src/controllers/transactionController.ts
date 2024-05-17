33d9a174d7f3cf8709d8a282816242303acba45c
Izayah Jordan <izayahjordan13@gmail.com>
2024-05-17 09:27:11 +0100

    added spaces to controller

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -9,7 +9,7 @@ export const makeTransaction = async (req: Request, res: Response) => {
     const corresponding_account: number = req.body.corresponding_account
 
     await addMoney(transaction_value, account_id)
-    if(await createTransaction(account_id, transaction_value, corresponding_account)){
+    if (await createTransaction(account_id, transaction_value, corresponding_account)){
         return res.status(201).send({'message': 'Transaction recorded'})
     } else {
        return res.status(401).send({'message': "Transaction Failed"})
@@ -19,9 +19,9 @@ export const makeTransaction = async (req: Request, res: Response) => {
 export const transactionHistory = async (req: Request, res: Response) => {
     const account_id: number = req.body.account_id
     const results = await retrieveTransactionHistory(account_id)
-    if(results){
+    if (results){
         return res.status(200).send({'data': results})
-    }else{
+    } else{
         return res.status(401).send({'message': "Failed to retrieve transaction history"})
     }
 }
